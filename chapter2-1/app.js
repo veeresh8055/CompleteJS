7d257627ad978fa482f62001e1325c29429407c0
Veeresh Chared <125709784+veeresh8055@users.noreply.github.com>
2025-05-07 19:03:17 +0530

    Add files via upload

---
+++ chapter2-1/app.js
@@ -0,0 +1,15 @@
+ // installing pretier 
+ // installing and setup the Live Seerver 
+ // installing Node Js 
+
+
+//  Documenting of Code 
+// 1. Line comments 
+// 2. Block comments 
+
+// imstalling Git :
+// » Modified means that you’ve changed a file but haven’t yet committed it to the
+// Git database.
+// » Staged means that you’ve marked a modified file to be a part your next
+// commit.
+// » Committed means that a file is stored in your local Git database.
