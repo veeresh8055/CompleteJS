83ea83e9bbeef93a1be53384c6e642f4f035125f
Veeresh Chared <125709784+veeresh8055@users.noreply.github.com>
2025-05-07 19:02:08 +0530

    Add files via upload

---
+++ Chapter1-1/app.js
@@ -0,0 +1,30 @@
+// In technical terms, JavaScript is a high-level, just-in-time compiled programming language. 
+
+// total 600pages
+// daily 10 pages 2.5 moths to to perfect in react and node [express js ] [postgrade..][mongo db ]
+let normalName = "veeresh's";
+let javaScriptName = normalName + " "+'JS';
+console.log('Your JavaScript Name is ' + javaScriptName);
+
+
+// JavaScript is made of statements
+// » Values: In Listing 1-1, 'Chris' is a value.
+// » Expressions: Expressions are units of code that are evaluated and become
+// values. For example, in Listing 1-1, the following is an expression:
+// normalName + 'Script'
+// When normalName has a value of 'Chris', this expression evaluates to
+// 'ChrisScript'.
+// » Operators: Operators do something with values. In Listing 1-1, the + operator
+// (known as the concatenation operator) joins together the value of normalName
+// and the literal value 'Script'.
+// » Keywords: Keywords are parts of JavaScript that have special meaning and
+// cause JavaScript to do something. In Listing 1-1, the word let is a keyword that
+// tells JavaScript to store the value on the right side of the = .
+
+// ~~~~~~IMP POINTS~~~~~~~~~~~~
+// 1. JavaScript is made of statements
+// 2. JavaScript is case-sensitive
+// 3. JavaScript ignores white space
+// 4. JavaScript programmers use camelCase and underscores
+
+// we can run it on website console 
