daab1ea57266dd282e712d9175260846dffc07a8
Veeresh Chared <125709784+veeresh8055@users.noreply.github.com>
2025-05-09 09:10:17 +0530

    Add files via upload

---
+++ chapter4-1/app.js
@@ -0,0 +1,60 @@
+// working with operators 
+// Assignment operators
+   let x = 10 
+   let y = 'veeresh'
+
+// Comparison operators
+// JavaScript Comparison Operators
+// Operator Description Example
+// ==    Equality                   3 == “3” // true
+// !=    Inequality                 3 != 3 // false
+// ===   Strict equality            3 === “3” // false
+// !==   Strict inequality          3 !== “3” // true
+// >     Greater than               7 > 1 // true
+// >=    Greater than or equal to   7 >= 7 // true
+// <     Less than                  7 < 10 // true
+// <=    Less than or equal to      2 <= 2 // true
+  
+console.log(0==='0') // false
+
+// Arithmetic operators
+
+// // Concatenation operator:
+// let yourName = "Marcellus L. Benfield";
+// let welcomeMessage = "Welcome, " + yourName;
+
+// Arithmetic Operators
+// Operator Description Example
+// +       Addition                   a = 1 + 1
+// –       Subtraction                a = 10 - 1
+// *       Multiplication             a = 2 * 2
+// /       Division                   a = 8 / 2
+// %       Remainder                  a = 5 % 2
+// ++      Increment                  a = ++b
+//                                    a = b++
+//                                    a++
+// --       Decrement                 a = --b
+//                                    a = b--
+//                                    a--
+// **       Exponentiation operator   2 ** 2
+let sum = 1+"1"; // result: "11"
+
+
+// Logical operators
+// Operator What It Means What It Does
+// &&   And      Returns the first falsy operand. If all values are truthy, it returns the value
+//               of the last operand.
+// ||   Or       Returns the value of the first truthy operand. If all the operands evaluate
+//               to false, it returns the last operand.
+// !    Not      Takes only one operand. Returns 
+let language = userPreference.language || 'English';
+
+// Combining operators
+score = score + 1;
+score += 1;
+
+
+// Other Operators
+
+// BITWISE OPERATOR
+ 
